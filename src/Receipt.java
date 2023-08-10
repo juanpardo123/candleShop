c2a492e4735f0b2ccfbbe66154dfa97952184359
juanpardo123 <129297821+juanpardo123@users.noreply.github.com>
2023-08-10 09:56:33 -0400

    final?

--- src/Receipt.java
+++ src/Receipt.java
@@ -1,10 +1,19 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Receipt {
+    Color backdrop = new Color(30,31,34,255);
     public Receipt(){
         JFrame receipt = new JFrame();
+        receipt.setBackground(backdrop);
         receipt.setSize(500,800);
         receipt.setVisible(true);
+        JPanel backing = new JPanel();
+        backing.setBackground(backdrop);
+        backing.setBounds(0,0,500,500);
+        receipt.add(backing);
+
+
 
     }
 }
